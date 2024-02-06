55cd104406715bbb7a16bad3d8b9d653d74e9473
Lucaturin2003 <159005233+Lucaturin2003@users.noreply.github.com>
2024-02-06 10:58:42 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace AccessModifier
+{
+    public class Vehicle
+    {
+        private readonly string _registrationNumber;
+        public Vehicle()
+        {
+            Console.WriteLine("Veicle is being initialized.");
+        }
+
+        public Vehicle(string registrationNumber)
+        {
+            _registrationNumber = registrationNumber;
+            Console.WriteLine("Veicle is being initialized " + registrationNumber);
+        }
+    }
+
+    public class Car: Vehicle
+    {
+        public Car()
+        {
+            Console.WriteLine("Car is being initialized");
+        }
+
+        public Car(string registrationNumber): base(registrationNumber)
+        {
+            Console.WriteLine("Car is being initialized " + registrationNumber);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var car = new Car("Registration");
+        }
+    }
+}
